0303325ada9ae551555a3acbfa5618b30503198c
marerokevin <marerokevin@gmail.com>
2026-03-14 11:38:17 +0800

    initial commit for nvim

---
+++ nvim/lua/theprimeagen/lazy/local.lua
@@ -0,0 +1,18 @@
+local local_plugins = {
+	--{
+	--    "cockpit",
+	--    dir = "~/personal/cockpit",
+	--    config = function()
+	--        require("cockpit")
+	--        vim.keymap.set("n", "<leader>ct", "<cmd>CockpitTest<CR>")
+	--        vim.keymap.set("n", "<leader>cr", "<cmd>CockpitRefresh<CR>")
+	--    end,
+	--},
+
+	-- {
+	-- 	"the-stru",
+	-- 	dir = "~/personal/the-stru",
+	-- },
+}
+
+return local_plugins
